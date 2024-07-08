ce267c7f0bfd5ac3fa8e5085156b6a26d396e615
ekadaegile <egileoniso.ekada@gmail.com>
2024-07-08 10:47:25 +0100

    fixing layout issues

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -19,6 +19,15 @@ const NavLink = ({name, link, sublink, external, isDisable }: INavlinks) => {
     const [isOpen, setIsOpen] = useState(false);
     const pathname = usePathname();
 
+
+    const clickHandler = (item: string) => {
+        if(show){
+            setShow("")
+        } else {
+            setShow(item)
+        }
+    }
+
     return (
         <div className="w-auto h-auto mb-4">
             <div className="flex items-center" onClick={() => setIsOpen(!isOpen)}>
