5bbbd0aedae8dea27fbd918cdf926627b979ed5e
ekadaegile <egileoniso.ekada@gmail.com>
2024-07-09 10:53:27 +0100

    update on navbar drawer

--- src/components/shared/landingpagelayout.tsx
+++ src/components/shared/landingpagelayout.tsx
@@ -1,6 +1,8 @@
+"use client"
 import React from 'react'
 import Navbar from './navbar'
 import Footer from '../footer' 
+import { usePathname } from 'next/navigation';
 
 export default function Landingpagelayout({
     children,
@@ -10,11 +12,14 @@ export default function Landingpagelayout({
     children: React.ReactNode;
     hidemena?: boolean,
     hidefooterinfo?: boolean
-}>) {
+}>) { 
+
+    const pathname = usePathname();
+
     return (
         <main className="flex w-full overflow-x-hidden h-auto flex-col items-center ">
             <Navbar />
-            <div className=" w-full lg:px-0 " >
+            <div className={` w-full lg:px-0  ${pathname === "/training/blog" ? "px-0" : "px-6"} `} >
                 {children}
                 <div className=" w-full lg:px-[94px] px-3 " >
                     <Footer />
