373c54f8cfc56741f1c3180288a30fc416d950b4
ekadaegile <egileoniso.ekada@gmail.com>
2024-07-31 01:34:46 +0100

    update on image placeholder

--- src/components/shared/blogData.tsx
+++ src/components/shared/blogData.tsx
@@ -1,47 +1,36 @@
-import { BLOG_DATA, IBlogData } from "@/common/BlogData";
-import { useRouter } from "next/navigation";
-import React from "react";
+import React from 'react'
 
 export default function BlogData() {
-  const items = BLOG_DATA.slice(0, 3);
 
-  const BlogCard = ({ item, index }: { item: IBlogData; index: number }) => {
-    const navigate = useRouter();
+    const BlogCard = () => {
+        return (
+            <div className=' w-full flex flex-col gap-6 ' >
+                <div className=' w-full h-[240px]  rounded-2xl ' >
+                    <img alt='blog' src='/images/blogimg.png' className=' w-full h-full object-cover rounded-2xl ' />
+                </div>
+                <div className=' w-full flex flex-col gap-2 ' >
+                    <p className=' text-sm leading-[22px] text-primary01 font-medium ' >Articles</p>
+                    <p className=' text-xl leading-[28px] font-semibold ' >Bible School Alumni Stories: Impacting the World for Christ</p>
+                    <p className=' leading-6 ' >Be inspired by the remarkable stories and achievements of our Bible school alumni, individuals who have gone on to...</p>
+                </div>
+                <div className=' flex items-center gap-3 ' >
+                    <div className=' rounded-full w-10 h-10 ' >
+                        <img alt='blog' src='/images/Avatar.png' className=' w-full h-full object-cover rounded-full ' />
+                    </div>
+                    <div className=' ' >
+                        <p className=' font-semibold text-sm leading-5 ' >Olivia Rhye</p>
+                        <p className=' text-sm leading-5 ' >20 Jan 2024</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
-      <div
-        className=" w-full flex flex-col gap-6 "
-        onClick={() => {
-          navigate.push(`/training/blog/${index.toString()}`);
-        }}
-      >
-        <div className=" w-full h-[240px] bg-violet-600 rounded-2xl overflow-hidden">
-          <img src={item?.image} alt="image" className="w-full h-full" />
+        <div className=' relative w-full flex lg:flex-row flex-col gap-4 mt-8  ' >
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
         </div>
-        <div className=" w-full flex flex-col gap-2 ">
-          <p className=" text-sm leading-[22px] text-primary01 font-medium ">
-            Articles
-          </p>
-          <p className=" text-xl leading-[28px] font-semibold ">{item.title}</p>
-          <p className=" leading-6 ">
-            {item.description.substring(0, 300) + "..."}
-          </p>
-        </div>
-        <div className=" flex items-center gap-3 ">
-          <div className=" bg-blue-800 rounded-full w-10 h-10 "></div>
-          <div className=" ">
-            <p className=" font-semibold text-sm leading-5 ">{item.author}</p>
-            {/* <p className=' text-sm leading-5 ' >20 Jan 2024</p> */}
-          </div>
-        </div>
-      </div>
-    );
-  };
-
-  return (
-    <div className=" relative w-full flex lg:flex-row flex-col gap-4 mt-8  ">
-      {items.map((item, index) => (
-        <BlogCard item={item} key={index.toString()} index={index} />
-      ))}
-    </div>
-  );
+    )
 }
